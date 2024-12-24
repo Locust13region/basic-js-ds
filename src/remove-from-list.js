423da75c6db42a54d7ac10050449828eeed30420
DESKTOP-MF8V5D1\Andrey <m>
2024-12-24 20:52:13 +0300

    feat: 8 passing

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require("../extensions/list-node.js");
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,11 +22,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+	// для всех элементов, кроме первого
+	currentItem = l;
+	while (currentItem.next) {
+		if (currentItem.next.value === k) {
+			currentItem.next = currentItem.next.next;
+		} else {
+			currentItem = currentItem.next;
+		}
+	}
+	// для первого элемента
+	if (l.value === k) {
+		l = l.next;
+	}
+
+	return l;
 }
 
 module.exports = {
-  removeKFromList
+	removeKFromList,
 };
